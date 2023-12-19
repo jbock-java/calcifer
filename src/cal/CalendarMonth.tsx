62490d026afcb5eefe6d693033c59444c740da59
jbock <jbock-java@gmx.de>
2023-12-19 14:02:38 +0100

    wochentage header hinzu

--- src/cal/CalendarMonth.tsx
+++ src/cal/CalendarMonth.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import { FC } from "react";
 import { DataDay, DataMonth, Week } from "../model/types";
 import { useAppSelector } from "../store/hooks";
+import { DayOfWeek } from "@js-joda/core";
 
 interface Parameters {
   month: DataMonth;
@@ -15,14 +16,29 @@ interface DayParameters {
   day: DataDay;
 }
 
+const grey = "text-slate-400";
+
 export const CalendarMonth: FC<Parameters> = ({ month }) => {
   const year = useAppSelector(store => store.calendar.year);
   const monthName = new Date(year, month.month.value() - 1, 15).toLocaleString('de', { month: 'long' });
+  const classes = classNames(
+    "font-mono",
+    "w-fit",
+    "grid",
+    "gap-x-2",
+    "grid-cols-[auto_auto_auto_auto_auto_auto_auto_auto]",
+  );
+  const dayHeader = [];
+  for (const d of ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]) {
+    dayHeader.push(<div className={classNames(grey, "justify-self-end")}>{d}</div>);
+  }
   return (
     <div>
-      <div className="font-mono w-fit grid gap-x-2 grid-cols-[auto_auto_auto_auto_auto_auto_auto_auto]">
+      <div className={classes}>
         <div></div>
-        <div className="col-span-7">{monthName} {year}</div>
+        <div className="col-span-7"><pre> {monthName} {year}</pre></div>
+        <div></div>
+        <>{dayHeader}</>
         {month.weeks.map(week =>
           <CalendarWeek key={'w-' + year + '-' + week.kw} week={week} />)}
       </div>
@@ -38,7 +54,7 @@ const CalendarWeek: FC<WeekParameters> = ({ week }) => {
   }
   return (
     <>
-      <div className="text-slate-400">{String(week.kw).padStart(2, '0')}</div>
+      <div className={grey}>{String(week.kw).padStart(2, '0')}</div>
       {days}
     </>);
 }
@@ -52,7 +68,8 @@ const CalendarDay: FC<DayParameters> = ({ day }) => {
   const classes = classNames(
     "px-1",
     "justify-self-end",
-    hl && "bg-yellow-300");
+    hl && "bg-yellow-300",
+  );
   const text = String(day.day?.dayOfMonth()).padStart(2, ' ');
   return <div className={classes}><pre>{text}</pre></div>
 }
