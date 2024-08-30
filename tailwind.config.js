8c7b56dfae4de83ce78f0a9af0bb321163fbc012
jbock <jbock-java@gmx.de>
2024-08-30 12:14:24 +0200

    dark mode selector

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/**/*.{js,jsx,ts,tsx}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  darkMode: "selector",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
-
